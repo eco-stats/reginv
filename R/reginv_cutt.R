d4e5cced3e306c07cda170d094af0e991c0162e0
David Warton <david.warton@unsw.edu.au>
2023-09-20 16:31:51 +1000

    minor help file change

--- R/reginv_cutt.R
+++ R/reginv_cutt.R
@@ -10,7 +10,7 @@
 #' @param sd Numeric vector of measurement error standard deviations for each fossil (listed in the same order as they appear in \code{ages}).
 #' @param K Numeric upper bound for fossil ages - how old fossils can be before they are ignored, for the purpose of this analysis. A sensible choice of \code{K} is
 #' close to the age of the oldest fossil.
-#' @param df Numeric; degrees of freedom for the t-distribution used to model measurement error. Must be at least 2. Default (Inf) uses a Gaussian distribution.
+#' @param df Numeric; degrees of freedom for the t-distribution used to model measurement error. Must be greater than 2. Default (Inf) uses a Gaussian distribution.
 #' @param alpha Numeric between 0 and 1. Used to find a 100(1-\code{alpha})\% confidence interval. Defaults to 0.05 (95\% confidence intervals)
 #' @param q Numeric vector of values between 0 and 1, specifying the quantiles at which we want to solve for extinction time. Defaults to \code{c(alpha/2,0.5,1-alpha/2)},
 #' which gives the limits of a 100(1-\code{alpha})\% confidence interval and a point estimate obtained by solving at 0.5. If \code{q} is specified it overrides any input for \code{alpha}.
